746cd110ee29a4193b49e19090b832f1237df9ae
koiralabishal <107185433+koiralabishal@users.noreply.github.com>
2022-06-13 16:35:15 +0545

    Create area 2.c

---
+++ area 2.c
@@ -0,0 +1,14 @@
+/*Finction without argument and return value*/
+#include<stdio.h>  
+void area();  
+void  main()  
+{  
+    area();   
+}  
+void area()  
+{  
+    int l,b;    
+    printf("\nEnter length and breadth:");  
+    scanf("%d %d",&l,&b);
+    printf("The area is : %d",l*b);  
+}  
